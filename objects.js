dd8cc7ece518c79efa3a9f3cc8c9cdfcad1fe2f9
simlaara <simlaara98@gmail.com>
2022-12-28 20:29:01 +0600

    js

---
+++ objects.js
@@ -0,0 +1,23 @@
+//objects basics with dot and bracket notation
+
+/*let name ='simla';
+let age ='24';
+let hobby='cooking';
+
+const user={
+    name:'simla',
+    age:'24',
+    hobby:'cooking',
+} ;
+document.write(user.hobby);
+document.write(user['name'])*/
+
+//Adding,modifying,deleteing property(dynamic property access);
+const user={
+    name:'simla',
+    age:'24',
+    hobby:'cooking',
+} ;
+user.email='simla@gmail.com';
+delete user.age;
+document.write(user.age);
---
+++ string.js
@@ -0,0 +1,40 @@
+/*let country ='Bangladesh';
+document.write(country);
+document.write(typeof country);
+document.write(country.charAt(5));//both same
+document.write(country[5]);//same
+document.write(country[12]);
+document.write(country.charAt(12));
+
+document.write(country.length);
+*/
+//let message ="please repeat the 'repeat' word ";
+//document.write(message.charAt(5));
+//document.write(message.charCodeAt(7));//for print unicode
+//document.write(message.toUpperCase());
+//document.write(message.toLowerCase());
+//document.write(message.includes('repeatly'));
+// document.write(message.startsWith('please',6));
+//document.write(message.endsWith('repeat,13'));
+//document.write(message.search('repeat'));
+//document.write(message.indexOf('repeat',14));
+//let temp =false;
+//document.write (typeof temp.toString());
+//document.write(temp.toString());
+/*let m1='shimla';
+let m2='sadk';
+let m3='turz0';
+ //let m=m1.concat(m2,m3);
+ let m = m1+m2+m3
+document.write(m);
+*/
+//let message ="please repeat the 'repeat' word ";
+//document.write(message.split('',2));
+
+//let message ="please repeat the 'repeat' word ";
+//document.write(message.slice(0,5));
+//document.write(message.slice(3,5));
+//document.write(message.slice(0,5));
+//document.write(message.slice(-5,1));
+//document.write(message.substring(0,5));
+//document.write(message.substring(0,13));
